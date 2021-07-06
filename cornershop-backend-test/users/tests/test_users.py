65481240bfc4fc02f63da444e8b25c986bafd69d
gabmichdev <gab.mich.dev@gmail.com>
2021-07-06 18:00:45 -0500

    Added feature to modify users and tests

--- cornershop-backend-test/users/tests/test_users.py
+++ cornershop-backend-test/users/tests/test_users.py
@@ -6,6 +6,7 @@ from rest_framework.test import APIClient
 
 CREATE_USER_URL = reverse("users:create")
 TOKEN_URL = reverse("users:token")
+ME_URL = reverse("users:me")
 
 
 def create_user(**kwargs):
@@ -88,3 +89,49 @@ class PublicUsersAPITests(TestCase):
 
         self.assertNotIn("token", res.data)
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_retrieve_user_unauthorized(self):
+        """Test that authentication is required for users"""
+        res = self.client.get(ME_URL)
+
+        self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
+
+
+class PrivateUsersAPITests(TestCase):
+    """Test API requests that require authentication"""
+
+    def setUp(self):
+        self.payload = {
+            "username": "testuser30",
+            "password": "test123.@1",
+            "name": "test",
+        }
+        self.user = create_user(**self.payload)
+        self.client = APIClient()
+        self.client.force_authenticate(user=self.user)
+
+    def test_retrieve_profile_success(self):
+        """Test retrieving profile for logged in user"""
+        res = self.client.get(ME_URL)
+
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(
+            res.data,
+            {"name": self.user.name, "username": self.user.username},
+        )
+
+    def test_post_me_not_allowed(self):
+        """Test that POST is not allowed on the me url"""
+        rest = self.client.post(ME_URL)
+
+        self.assertEqual(rest.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
+
+    def test_update_user_profile(self):
+        """Test updating the user profile for authenticated user"""
+        payload = {"name": "Jose Gabriel Michel", "password": "new_pass123"}
+        res = self.client.patch(ME_URL, payload)
+
+        self.user.refresh_from_db()
+        self.assertEqual(self.user.name, payload["name"])
+        self.assertTrue(self.user.check_password(payload["password"]))
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
